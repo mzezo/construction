841b8dd81c23fa388f95494c0cbfe4e7df56cfc4
mzezo <mohamed.abdelaziz.khalel@gmail.com>
2022-06-23 01:09:22 +0200

    add blogs api

--- src/components/BlogItem/BlogItem.tsx
+++ src/components/BlogItem/BlogItem.tsx
@@ -5,16 +5,16 @@ import { motion } from 'framer-motion'
 import { fadeInUp } from "@/utils/Motion/FadeInUp";
 
 interface BlogItemType {
-  title: any,
+  name: string,
   description: any,
-  imgSrc: any,
+  image: any,
   postedBy: any,
   date: any,
   likes: any,
   comments: any,
 }
 
-function BlogItem({ title, description, imgSrc, postedBy, date, likes, comments }: BlogItemType) {
+function BlogItem({ name, description, image, postedBy, date, likes, comments }: BlogItemType) {
 
   const controls = useAnimation();
   const [ref, inView] = useInView();
@@ -35,26 +35,26 @@ function BlogItem({ title, description, imgSrc, postedBy, date, likes, comments 
       <div className="blog-post blog-grid blog-rounded radius-sm shadow">
         <div className="dlab-post-media dlab-img-effect">
           <a href="blog-details.html">
-            <img src={imgSrc || "/images/blog/blog/pic1.jpg"} alt="" />
+            <img src={image?.data?.attributes?.url || "/images/blog/blog/pic1.jpg"} alt="" className="h-[320px]" />
           </a>
         </div>
         <div className="dlab-info p-lr50 p-tb40">
           <div className="dlab-post-meta">
             <ul className="d-flex align-items-center">
               <li className="post-date"> {date} </li>
-              <li className="post-author">
+              {/* <li className="post-author">
                 <i className="ti ti-thumb-up"></i>{" "}
                 <a href="javascript:void(0);">{likes}</a>{" "}
-              </li>
-              <li className="post-comment">
+              </li> */}
+              {/* <li className="post-comment">
                 <i className="ti ti-comments"></i>{" "}
                 <a href="javascript:void(0);">{comments}</a>{" "}
-              </li>
+              </li> */}
             </ul>
           </div>
           <div className="dlab-post-title">
             <h4 className="post-title truncate">
-              <a href="blog-details.html">{title}</a>
+              <a href="blog-details.html">{name}</a>
             </h4>
           </div>
           <div className="dlab-post-text">
@@ -62,12 +62,12 @@ function BlogItem({ title, description, imgSrc, postedBy, date, likes, comments 
               {description}
             </p>
           </div>
-          <div className="dlab-post-name">
+          {/* <div className="dlab-post-name">
             Posted by{" "}
             <a href="#" className="site-button-link">
               {postedBy}
             </a>
-          </div>
+          </div> */}
         </div>
       </div>
     </motion.div>
--- src/pages/blogs.tsx
+++ src/pages/blogs.tsx
@@ -1,18 +1,30 @@
 import BlogItem from "@/components/BlogItem/BlogItem"
 import data from "@/components/BlogItem/fakeData.json"
+import { getData } from "@/services/getData"
+import { useEffect, useState } from "react"
 
 function BlogGrid() {
+
+  const [blogs, setBlogs] = useState<any>([]);
+
+
+  useEffect(() => {
+    getData('/blogs?populate=*').then((data) => { 
+      setBlogs(data?.data?.data)
+    })
+  }, [])
+  
   return (
     <div className="content-block">
       <div className="section-full content-inner">
         <div className="container">
           <div className="dlab-blog-grid-3 row" id="masonry">
-            {data.map((ele, index) => (
-              <BlogItem key={index} {...ele} />
+            {blogs.map((ele: any) => (
+              <BlogItem key={ele?.id} {...ele?.attributes} />
             ))}
           </div>
 
-          <div className="pagination-bx radius clearfix text-center">
+          {/* <div className="pagination-bx radius clearfix text-center">
             <ul className="pagination">
               <li className="active">
                 <a href="javascript:void(0);">1</a>
@@ -27,7 +39,7 @@ function BlogGrid() {
                 <a href="javascript:void(0)">18</a>
               </li>
             </ul>
-          </div>
+          </div> */}
         </div>
       </div>
     </div>
