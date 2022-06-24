7ec0f7e487d5ae3db4993a805b17955714dcc4c5
mzezo <mohamed.abdelaziz.khalel@gmail.com>
2022-06-25 00:41:45 +0200

    comment blog details extra

--- src/components/BlogDetails/BlogDetail.tsx
+++ src/components/BlogDetails/BlogDetail.tsx
@@ -1,80 +1,80 @@
 import ReactMarkdown from "react-markdown";
 
-function AuthorBlock() {
-  return (
-    <div className="author-box blog-user m-b60">
-      <div className="author-profile-info">
-        <div className="author-profile-pic">
-          <img src="/images/testimonials/pic1.jpg" alt="" />
-        </div>
-        <div className="author-profile-content">
-          <h5>John Doe</h5>
+// function AuthorBlock() {
+//   return (
+//     <div className="author-box blog-user m-b60">
+//       <div className="author-profile-info">
+//         <div className="author-profile-pic">
+//           <img src="/images/testimonials/pic1.jpg" alt="" />
+//         </div>
+//         <div className="author-profile-content">
+//           <h5>John Doe</h5>
 
-          <p>
-            Lorem ipsum dolor sit amet consectetur adipiscing elit. Vestibulum
-            ac vehicula leo dulam nullam rhoncus tortor at dignissim vehicula.
-            Vestibulum ac vehicula leo dulam nullam rhoncus tortor at dignissim
-            vehicula.
-          </p>
-          <ul className="list-inline m-b0">
-            <li>
-              <a href="javascript:void(0);" className="btn-link">
-                <i className="fab fa-facebook-f"></i>
-              </a>
-            </li>
-            <li>
-              <a href="javascript:void(0);" className="btn-link">
-                <i className="fab fa-google-plus-g"></i>
-              </a>
-            </li>
-            <li>
-              <a href="javascript:void(0);" className="btn-link">
-                <i className="fab fa-instagram"></i>
-              </a>
-            </li>
-            <li>
-              <a href="javascript:void(0);" className="btn-link">
-                <i className="fab fa-twitter"></i>
-              </a>
-            </li>
-          </ul>
-        </div>
-      </div>
-    </div>
-  )
-}
+//           <p>
+//             Lorem ipsum dolor sit amet consectetur adipiscing elit. Vestibulum
+//             ac vehicula leo dulam nullam rhoncus tortor at dignissim vehicula.
+//             Vestibulum ac vehicula leo dulam nullam rhoncus tortor at dignissim
+//             vehicula.
+//           </p>
+//           <ul className="list-inline m-b0">
+//             <li>
+//               <a href="javascript:void(0);" className="btn-link">
+//                 <i className="fab fa-facebook-f"></i>
+//               </a>
+//             </li>
+//             <li>
+//               <a href="javascript:void(0);" className="btn-link">
+//                 <i className="fab fa-google-plus-g"></i>
+//               </a>
+//             </li>
+//             <li>
+//               <a href="javascript:void(0);" className="btn-link">
+//                 <i className="fab fa-instagram"></i>
+//               </a>
+//             </li>
+//             <li>
+//               <a href="javascript:void(0);" className="btn-link">
+//                 <i className="fab fa-twitter"></i>
+//               </a>
+//             </li>
+//           </ul>
+//         </div>
+//       </div>
+//     </div>
+//   )
+// }
 
-function BlogHeader() {
-  return (
-    <div className="dlab-post-info">
-      <div className="dlab-post-title">
-        <h2 className="post-title">
-          Magazine Charts the Most Fashionable New York Women in 2017
-        </h2>
-      </div>
-      <div className="dlab-post-meta">
-        <ul className="d-flex align-items-center">
-          <li className="dlab-post-name">
-            {" "}
-            <img src="/images/testimonials/pic3.jpg" alt="" /> Posted by{" "}
-            <a href="javascript:void(0)" className="site-button-link">
-              Admin
-            </a>
-          </li>
-          <li className="post-date"> 24 Sep 2017 </li>
-          <li className="post-author">
-            <i className="ti ti-thumb-up"></i>{" "}
-            <a href="javascript:void(0)">18</a>{" "}
-          </li>
-          <li className="post-comment">
-            <i className="ti ti-comments"></i>{" "}
-            <a href="javascript:void(0)">9</a>{" "}
-          </li>
-        </ul>
-      </div>
-    </div>
-  )
-}
+// function BlogHeader() {
+//   return (
+//     <div className="dlab-post-info">
+//       <div className="dlab-post-title">
+//         <h2 className="post-title">
+//           Magazine Charts the Most Fashionable New York Women in 2017
+//         </h2>
+//       </div>
+//       <div className="dlab-post-meta">
+//         <ul className="d-flex align-items-center">
+//           <li className="dlab-post-name">
+//             {" "}
+//             <img src="/images/testimonials/pic3.jpg" alt="" /> Posted by{" "}
+//             <a href="javascript:void(0)" className="site-button-link">
+//               Admin
+//             </a>
+//           </li>
+//           <li className="post-date"> 24 Sep 2017 </li>
+//           <li className="post-author">
+//             <i className="ti ti-thumb-up"></i>{" "}
+//             <a href="javascript:void(0)">18</a>{" "}
+//           </li>
+//           <li className="post-comment">
+//             <i className="ti ti-comments"></i>{" "}
+//             <a href="javascript:void(0)">9</a>{" "}
+//           </li>
+//         </ul>
+//       </div>
+//     </div>
+//   )
+// }
 
 
 // TODO: sync data
