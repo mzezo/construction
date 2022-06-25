d8ed87a1a8df06581527b1469d32785b29fbc699
mzezo <mohamed.abdelaziz.khalel@gmail.com>
2022-06-25 11:33:20 +0200

    handle delete all js files

--- next-i18next.config.js
+++ next-i18next.config.js
@@ -1,9 +1,10 @@
 const path = require("path");
+
 module.exports = {
-	i18n: {
-		locales: ["en", "ar"],
-		defaultLocale: "en",
-		localeDetection: false,
-	},
-	localePath: path.resolve("./public/locales"),
+  i18n: {
+    locales: ["en", "ar"],
+    defaultLocale: "en",
+    localeDetection: false,
+  },
+  localePath: path.resolve("./public/locales"),
 };
