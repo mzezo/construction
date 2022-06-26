f4cdcbdbeb7ed28034ae0377644eac89c29dc174
Hassan Ezz <dhassanezz98@gmail.com>
2022-06-26 21:52:30 +0200

    project details api integration

--- src/components/BlogEnteries/BlogEnteries.tsx
+++ src/components/BlogEnteries/BlogEnteries.tsx
@@ -1,4 +1,6 @@
 import { getData } from "@/services/getData";
+import { useTranslation } from "next-i18next";
+import { useRouter } from "next/router";
 import { useState, useEffect } from "react";
 import BlogEntry from "./BlogEntry"
 
@@ -6,17 +8,23 @@ function BlogEnteries() {
 
   const [blogs, setBlogs] = useState<any>([]);
 
+  const { t } = useTranslation("common")
+
+  const router = useRouter()
+  const { locale } = router
+
   useEffect(() => {
-    getData('/blogs?populate=*').then((data) => { 
+    getData(`/blogs?populate=*&locale=${locale}`).then((data) => { 
       setBlogs(data?.data?.data)
     })
   }, [])
+
   return (
     <div className="section-full bg-white content-inner-1">
       <div className="container">
         <div className="section-head text-center">
           <h2 className="head-title m-b10">Blog Enteries</h2>
-          <p>19+ years’ experience</p>
+          <p>19+ {t("years-experience")}</p>
         </div>
 
         <div className="blog-carousel grid grid-cols-2 gap-5 md:gap-8 md:grid-cols-3">
--- src/pages/project-details.tsx
+++ src/pages/project-details.tsx
@@ -1,6 +1,128 @@
+import { useEffect, useState } from "react"
 import FadeInAnimate from "@/components/UI/FadeInAnimate"
+import { getData } from "@/services/getData"
+import { useTranslation } from "next-i18next"
+import { useRouter } from "next/router"
+
+function Project({ projectData }: any) {
+
+  const { t } = useTranslation("common")
+
+  return (
+    <div className="section-full content-inner">
+      <div className="container">
+        <div className="row">
+          <div className="col-lg-5 m-b30">
+            <div className="section-head style1">
+              <p className="pre-title">YEAR {projectData.date.split("-")[0]}</p>
+              <h2>
+                {projectData.name}
+              </h2>
+            </div>
+            <p className="m-b30">
+              Meh synth Schlitz, tempor duis single-origin coffee ea next level
+              ethnic fingerstache fanny pack nostrud. Photo booth anim 8-bit
+              hella, PBR 3 wolf moon beard Helvetica. Salvia esse nihil,
+              flexitarian Truffaut synth art party deep v chillwave. Seitan High
+              Life reprehenderit consectetur cupidatat kogi. Et leggings fanny
+              pack.
+            </p>
+            <p className="m-b30">
+              {projectData.description}
+            </p>
+            <a
+              href="https://www.youtube.com/watch?v=Dj6CKxQue7U"
+              className="popup-youtube m-r20 video btn outline outline-2 button-lg black radius-xl btn-aware btn-video"
+            >
+              <i className="fas fa-play"></i>
+              <span></span>
+            </a>
+            <a
+              href="contact-us-1.html"
+              className="btn outline outline-2 button-lg black radius-xl btn-aware"
+            >
+              {t("contact-us")}
+              <span></span>
+            </a>
+          </div>
+          <div className="col-lg-7">
+            <div
+              className="m-b30 wow fadeIn mfp-gallery"
+              data-wow-duration="2s"
+              data-wow-delay="0.2s"
+            >
+              <div className="dlab-box portfolio-bx style2 project-media">
+                <div className="dlab-media dlab-img-overlay1 dlab-img-effect">
+                  <a href="javascript:void(0);">
+                    {" "}
+                    <img src="/images/projects/img3.jpg" alt="" />{" "}
+                  </a>
+                  <div className="overlay-bx">
+                    <a
+                      href="images/projects/img3.jpg"
+                      className="mfp-link"
+                      title="Title Come Here"
+                    >
+                      <i className="ti-zoom-in"></i>
+                    </a>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div className="row widget widget_getintuch widget_getintuch-pro-details m-lr0">
+              <div className="col-xl-4 col-lg-6 col-md-4 col-sm-6 p-lr0">
+                <div className="pro-details">
+                  <i className="ti ti-user"></i>
+                  <strong>CLIENT</strong> {projectData.client}
+                </div>
+              </div>
+              <div className="col-xl-4 col-lg-6 col-md-4 col-sm-6 p-lr0">
+                <div className="pro-details">
+                  <i className="ti ti-user"></i>
+                  <strong>ARCHITECT</strong>Jimmy Smith
+                </div>
+              </div>
+              <div className="col-xl-4 col-lg-6 col-md-4 col-sm-6 p-lr0">
+                <div className="pro-details">
+                  <i className="ti ti-location-pin"></i>
+                  <strong>LOCATION</strong> {projectData.location}
+                </div>
+              </div>
+              <div className="col-xl-4 col-lg-6 col-md-4 col-sm-6 p-lr0">
+                <div className="pro-details">
+                  <i className="ti ti-ruler-alt-2"></i>
+                  <strong>SIZE</strong>1,200m<sup>2</sup>
+                </div>
+              </div>
+              <div className="col-xl-4 col-lg-6 col-md-4 col-sm-6 p-lr0">
+                <div className="pro-details">
+                  <i className="ti ti-home"></i>
+                  <strong>TYPE</strong>Residential Project
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
 
 function ProjectDetails() {
+  const [projects, setProjects] = useState<any>([])
+
+  const { t } = useTranslation("common")
+
+  const router = useRouter()
+  const { locale } = router
+
+  useEffect(() => {
+    getData(`/projects?populate=*&locale=${locale}`).then((data) => {
+      setProjects(data?.data?.data)
+      console.log(projects)
+    })
+  }, [])
+
   return (
     <div className="page-content">
       <div className="section-full content-inner-2">
@@ -28,6 +150,7 @@ function ProjectDetails() {
           </div>
         </div>
       </div>
+
       <FadeInAnimate
         className="section-full content-inner-2 overlay-primary-middle wow fadeIn"
         style={{
@@ -114,107 +237,10 @@ function ProjectDetails() {
           </div>
         </div>
       </div>
-      <div className="section-full content-inner">
-        <div className="container">
-          <div className="row">
-            <div className="col-lg-5 m-b30">
-              <div className="section-head style1">
-                <p className="pre-title">YEAR 2018</p>
-                <h2>
-                  Creating authentic and Character for most Luxury Residential
-                  project
-                </h2>
-              </div>
-              <p className="m-b30">
-                Meh synth Schlitz, tempor duis single-origin coffee ea next
-                level ethnic fingerstache fanny pack nostrud. Photo booth anim
-                8-bit hella, PBR 3 wolf moon beard Helvetica. Salvia esse nihil,
-                flexitarian Truffaut synth art party deep v chillwave. Seitan
-                High Life reprehenderit consectetur cupidatat kogi. Et leggings
-                fanny pack.
-              </p>
-              <p className="m-b40">
-                Exercitation photo booth stumptown tote bag Banksy, elit small
-                batch freegan sed. Craft beer elit seitan exercitation, photo
-                booth et 8-bit kale chips proident chillwave deep v laborum.
-                Aliquip veniam delectus, Marfa eiusmod Pinterest in do umami
-                readymade swag. Selfies iPhone Kickstarter, drinking vinegar
-                jean vinegar stumptown yr pop-up artisan.
-              </p>
-              <a
-                href="https://www.youtube.com/watch?v=Dj6CKxQue7U"
-                className="popup-youtube m-r20 video btn outline outline-2 button-lg black radius-xl btn-aware btn-video"
-              >
-                <i className="fas fa-play"></i>
-                <span></span>
-              </a>
-              <a
-                href="contact-us-1.html"
-                className="btn outline outline-2 button-lg black radius-xl btn-aware"
-              >
-                Contact us<span></span>
-              </a>
-            </div>
-            <div className="col-lg-7">
-              <div
-                className="m-b30 wow fadeIn mfp-gallery"
-                data-wow-duration="2s"
-                data-wow-delay="0.2s"
-              >
-                <div className="dlab-box portfolio-bx style2 project-media">
-                  <div className="dlab-media dlab-img-overlay1 dlab-img-effect">
-                    <a href="javascript:void(0);">
-                      {" "}
-                      <img src="/images/projects/img3.jpg" alt="" />{" "}
-                    </a>
-                    <div className="overlay-bx">
-                      <a
-                        href="images/projects/img3.jpg"
-                        className="mfp-link"
-                        title="Title Come Here"
-                      >
-                        <i className="ti-zoom-in"></i>
-                      </a>
-                    </div>
-                  </div>
-                </div>
-              </div>
-              <div className="row widget widget_getintuch widget_getintuch-pro-details m-lr0">
-                <div className="col-xl-4 col-lg-6 col-md-4 col-sm-6 p-lr0">
-                  <div className="pro-details">
-                    <i className="ti ti-user"></i>
-                    <strong>CLIENT</strong> Martin Stewart
-                  </div>
-                </div>
-                <div className="col-xl-4 col-lg-6 col-md-4 col-sm-6 p-lr0">
-                  <div className="pro-details">
-                    <i className="ti ti-user"></i>
-                    <strong>ARCHITECT</strong>Jimmy Smith
-                  </div>
-                </div>
-                <div className="col-xl-4 col-lg-6 col-md-4 col-sm-6 p-lr0">
-                  <div className="pro-details">
-                    <i className="ti ti-location-pin"></i>
-                    <strong>LOCATION</strong>London, UK
-                  </div>
-                </div>
-                <div className="col-xl-4 col-lg-6 col-md-4 col-sm-6 p-lr0">
-                  <div className="pro-details">
-                    <i className="ti ti-ruler-alt-2"></i>
-                    <strong>SIZE</strong>1,200m<sup>2</sup>
-                  </div>
-                </div>
-                <div className="col-xl-4 col-lg-6 col-md-4 col-sm-6 p-lr0">
-                  <div className="pro-details">
-                    <i className="ti ti-home"></i>
-                    <strong>TYPE</strong>Residential Project
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
+
+      {projects.map((ele: any, index: number) => (
+        <Project key={index} projectData={ele.attributes}  />
+      ))}
     </div>
   )
 }
