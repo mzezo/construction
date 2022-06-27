c3db4759681848203d224a32b33a15de9dcccb95
mzezo <mohamed.abdelaziz.khalel@gmail.com>
2022-06-27 19:00:17 +0200

    add react query

---
+++ src/services/getHomeBanners.ts
@@ -0,0 +1,16 @@
+
+import { useQuery } from "react-query";
+import { getData } from "./getData";
+
+export const fetchHomeBanners = async ({ queryKey }: any, locale: string) => {
+	const [_key, _params] = queryKey;
+	const resData = await  getData(`/home-banners?populate=*&locale=${locale}`);
+	const { data } = resData;
+	return data?.data as any[] ;
+};
+export const useHomeBannersQuery = (options: any, locale: string) => {
+	return useQuery<any[], Error>(
+		['HomeBanners', locale],
+		(data) => fetchHomeBanners(data, locale)
+	);
+};
