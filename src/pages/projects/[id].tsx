b166ef3df5cac9bbcf9e30e4c4821052ee81822a
mzezo <mohamed.abdelaziz.khalel@gmail.com>
2022-07-13 02:06:09 +0200

    project details

--- src/pages/projects/[id].tsx
+++ src/pages/projects/[id].tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react"
+import { Fragment, useEffect, useState } from "react"
 import FadeInAnimate from "@/components/UI/FadeInAnimate"
 import { getData } from "@/services/getData"
 import { useTranslation } from "next-i18next"
@@ -11,99 +11,104 @@ function Project({ projectData }: any) {
   const { t } = useTranslation("common")
 
   return (
-    <div className="section-full content-inner">
-      <div className="container">
-        <div className="row">
-          <div className="col-lg-5 m-b30">
-            <div className="section-head style1">
-              <p className="pre-title">YEAR {projectData.date.split("-")[0]}</p>
-              <h2>{projectData.name}</h2>
+    <Fragment>
+      <div className="section-full content-inner">
+        <div className="container">
+          <div className="row">
+            <div className="col-lg-5 m-b30">
+              <div className="section-head style1">
+                <p className="pre-title">YEAR {projectData.date.split("-")[0]}</p>
+                <h2>{projectData.name}</h2>
+              </div>
+              <p className="m-b30">
+                <ReactMarkdown children={projectData.description} />
+              </p>
+              <a
+                href="https://www.youtube.com/watch?v=Dj6CKxQue7U"
+                className="popup-youtube m-r20 video btn outline outline-2 button-lg black radius-xl btn-aware btn-video"
+              >
+                <i className="fas fa-play"></i>
+                <span></span>
+              </a>
+              <a
+                href="contact-us-1.html"
+                className="btn outline outline-2 button-lg black radius-xl btn-aware"
+              >
+                {t("contact-us")}
+                <span></span>
+              </a>
             </div>
-            <p className="m-b30">
-              <ReactMarkdown children={projectData.description} />
-            </p>
-            <a
-              href="https://www.youtube.com/watch?v=Dj6CKxQue7U"
-              className="popup-youtube m-r20 video btn outline outline-2 button-lg black radius-xl btn-aware btn-video"
-            >
-              <i className="fas fa-play"></i>
-              <span></span>
-            </a>
-            <a
-              href="contact-us-1.html"
-              className="btn outline outline-2 button-lg black radius-xl btn-aware"
-            >
-              {t("contact-us")}
-              <span></span>
-            </a>
-          </div>
-          <div className="col-lg-7">
-            <div
-              className="m-b30 wow fadeIn mfp-gallery"
-              data-wow-duration="2s"
-              data-wow-delay="0.2s"
-            >
-              <div className="dlab-box portfolio-bx style2 project-media">
-                <div className="dlab-media dlab-img-overlay1 dlab-img-effect">
-                  <a href="javascript:void(0);">
-                    {" "}
-                    <img src="/images/projects/img3.jpg" alt="" />{" "}
-                  </a>
-                  <div className="overlay-bx">
-                    <a
-                      href="images/projects/img3.jpg"
-                      className="mfp-link"
-                      title="Title Come Here"
-                    >
-                      <i className="ti-zoom-in"></i>
+            <div className="col-lg-7">
+              <div
+                className="m-b30 wow fadeIn mfp-gallery"
+                data-wow-duration="2s"
+                data-wow-delay="0.2s"
+              >
+                <div className="dlab-box portfolio-bx style2 project-media">
+                  <div className="dlab-media dlab-img-overlay1 dlab-img-effect ">
+                    <a href="javascript:void(0);">
+                      <img
+                        src={projectData.image.data.attributes.url}
+                        className="object-cover aspect-square w-full"
+                        alt=""
+                      />
                     </a>
+                    <div className="overlay-bx">
+                      <a
+                        href={projectData.image.data.attributes.url}
+                        className="mfp-link"
+                        title="Title Come Here"
+                      >
+                        <i className="ti-zoom-in"></i>
+                      </a>
+                    </div>
                   </div>
                 </div>
               </div>
-            </div>
-            <div className="row widget widget_getintuch widget_getintuch-pro-details m-lr0">
-              <div className="col-xl-4 col-lg-6 col-md-4 col-sm-6 p-lr0">
-                <div className="pro-details">
-                  <i className="ti ti-user"></i>
-                  <strong>CLIENT</strong> {projectData.client}
+              <div className="row widget widget_getintuch widget_getintuch-pro-details m-lr0">
+                <div className="col-xl-4 col-lg-6 col-md-4 col-sm-6 p-lr0">
+                  <div className="pro-details">
+                    <i className="ti ti-user"></i>
+                    <strong>CLIENT</strong> {projectData.client}
+                  </div>
                 </div>
-              </div>
-              <div className="col-xl-4 col-lg-6 col-md-4 col-sm-6 p-lr0">
-                <div className="pro-details">
-                  <i className="ti ti-user"></i>
-                  <strong>ARCHITECT</strong>Jimmy Smith
+                <div className="col-xl-4 col-lg-6 col-md-4 col-sm-6 p-lr0">
+                  <div className="pro-details">
+                    <i className="ti ti-user"></i>
+                    <strong>ARCHITECT</strong>Jimmy Smith
+                  </div>
                 </div>
-              </div>
-              <div className="col-xl-4 col-lg-6 col-md-4 col-sm-6 p-lr0">
-                <div className="pro-details">
-                  <i className="ti ti-location-pin"></i>
-                  <strong>LOCATION</strong> {projectData.location}
+                <div className="col-xl-4 col-lg-6 col-md-4 col-sm-6 p-lr0">
+                  <div className="pro-details">
+                    <i className="ti ti-location-pin"></i>
+                    <strong>LOCATION</strong> {projectData.location}
+                  </div>
                 </div>
-              </div>
-              <div className="col-xl-4 col-lg-6 col-md-4 col-sm-6 p-lr0">
-                <div className="pro-details">
-                  <i className="ti ti-ruler-alt-2"></i>
-                  <strong>SIZE</strong>1,200m<sup>2</sup>
+                <div className="col-xl-4 col-lg-6 col-md-4 col-sm-6 p-lr0">
+                  <div className="pro-details">
+                    <i className="ti ti-ruler-alt-2"></i>
+                    <strong>SIZE</strong>1,200m<sup>2</sup>
+                  </div>
                 </div>
-              </div>
-              <div className="col-xl-4 col-lg-6 col-md-4 col-sm-6 p-lr0">
-                <div className="pro-details">
-                  <i className="ti ti-home"></i>
-                  <strong>TYPE</strong>Residential Project
+                <div className="col-xl-4 col-lg-6 col-md-4 col-sm-6 p-lr0">
+                  <div className="pro-details">
+                    <i className="ti ti-home"></i>
+                    <strong>TYPE</strong>Residential Project
+                  </div>
                 </div>
               </div>
             </div>
           </div>
         </div>
       </div>
-    </div>
+    </Fragment>
   )
 }
 
 function ProjectDetails() {
   const [project, setProject] = useState<any>({})
 
-  // const { t } = useTranslation("common")
+  const { t } = useTranslation("common")
 
   const router = useRouter()
   const {
@@ -120,127 +125,78 @@ function ProjectDetails() {
   }, [id])
 
   return (
-    <div className="page-content">
-      <div className="section-full content-inner-2">
-        <div className="container">
-          <div className="row">
-            <FadeInAnimate className="col-lg-12 wow fadeInUp mfp-gallery">
-              <div className="dlab-box portfolio-bx style2 project-media">
-                <div className="dlab-media dlab-img-overlay1 dlab-img-effect">
-                  <a href="javascript:void(0);">
-                    {" "}
-                    <img src="/images/projects/img1.jpg" alt="" />{" "}
-                  </a>
-                  <div className="overlay-bx">
-                    <a
-                      href="images/projects/img1.jpg"
-                      className="mfp-link"
-                      title="Title Come Here"
-                    >
-                      <i className="ti-zoom-in"></i>
+    <Fragment>
+      <div className="page-content">
+        <div className="section-full content-inner-1">
+          <div className="container">
+            <div className="row our-gallery mfp-gallery">
+              <FadeInAnimate className="col-lg-12 col-md-12 col-sm-12 m-b30 wow fadeInUp">
+                <div className="dlab-box portfolio-bx style2 project-media">
+                  <div className="dlab-media dlab-img-overlay1 dlab-img-effect">
+                    <a href="javascript:void(0);">
+                      {" "}
+                      <img src="/images/gallery/pic5.jpg" alt="" />{" "}
                     </a>
+                    <div className="overlay-bx">
+                      <a
+                        href="images/gallery/pic5.jpg"
+                        className="mfp-link"
+                        title="Title Come Here"
+                      >
+                        <i className="ti-zoom-in"></i>
+                      </a>
+                    </div>
                   </div>
                 </div>
-              </div>
-            </FadeInAnimate>
-          </div>
-        </div>
-      </div>
-
-      <FadeInAnimate
-        className="section-full content-inner-2 overlay-primary-middle wow fadeIn"
-        style={{
-          backgroundImage: "url(/images/projects/img2.jpg)",
-          backgroundSize: "cover",
-        }}
-      >
-        <div className="container">
-          <div className="row">
-            <div className="col-lg-12 video-bx">
-              <div className="video-play play-bg-primary">
-                <a
-                  href="https://www.youtube.com/watch?v=Dj6CKxQue7U"
-                  className="popup-youtube video"
-                >
-                  <i className="fas fa-play"></i>
-                </a>
-              </div>
+              </FadeInAnimate>
+              <FadeInAnimate className="col-lg-6 col-md-6 col-sm-6 m-b30 wow fadeInLeft">
+                <div className="dlab-box portfolio-bx style2 project-media">
+                  <div className="dlab-media dlab-img-overlay1 dlab-img-effect">
+                    <a href="javascript:void(0);">
+                      {" "}
+                      <img src="/images/gallery/gallery-6/pic3.jpg" alt="" />{" "}
+                    </a>
+                    <div className="overlay-bx">
+                      <a
+                        href="images/gallery/gallery-6/pic3.jpg"
+                        className="mfp-link"
+                        title="Title Come Here"
+                      >
+                        <i className="ti-zoom-in"></i>
+                      </a>
+                    </div>
+                  </div>
+                </div>
+              </FadeInAnimate>
+              <FadeInAnimate className="col-lg-6 col-md-6 col-sm-6 m-b30 wow fadeInRight">
+                <div className="dlab-box portfolio-bx style2 project-media">
+                  <div className="dlab-media dlab-img-overlay1 dlab-img-effect">
+                    <a href="javascript:void(0);">
+                      {" "}
+                      <img src="/images/gallery/gallery-6/pic4.jpg" alt="" />{" "}
+                    </a>
+                    <div className="overlay-bx">
+                      <a
+                        href="images/gallery/gallery-6/pic4.jpg"
+                        className="mfp-link"
+                        title="Title Come Here"
+                      >
+                        <i className="ti-zoom-in"></i>
+                      </a>
+                    </div>
+                  </div>
+                </div>
+              </FadeInAnimate>
             </div>
           </div>
         </div>
-      </FadeInAnimate>
-
-      <div className="section-full content-inner-1">
-        <div className="container">
-          <div className="row our-gallery mfp-gallery">
-            <FadeInAnimate className="col-lg-12 col-md-12 col-sm-12 m-b30 wow fadeInUp">
-              <div className="dlab-box portfolio-bx style2 project-media">
-                <div className="dlab-media dlab-img-overlay1 dlab-img-effect">
-                  <a href="javascript:void(0);">
-                    {" "}
-                    <img src="/images/gallery/pic5.jpg" alt="" />{" "}
-                  </a>
-                  <div className="overlay-bx">
-                    <a
-                      href="images/gallery/pic5.jpg"
-                      className="mfp-link"
-                      title="Title Come Here"
-                    >
-                      <i className="ti-zoom-in"></i>
-                    </a>
-                  </div>
-                </div>
-              </div>
-            </FadeInAnimate>
-            <FadeInAnimate className="col-lg-6 col-md-6 col-sm-6 m-b30 wow fadeInLeft">
-              <div className="dlab-box portfolio-bx style2 project-media">
-                <div className="dlab-media dlab-img-overlay1 dlab-img-effect">
-                  <a href="javascript:void(0);">
-                    {" "}
-                    <img src="/images/gallery/gallery-6/pic3.jpg" alt="" />{" "}
-                  </a>
-                  <div className="overlay-bx">
-                    <a
-                      href="images/gallery/gallery-6/pic3.jpg"
-                      className="mfp-link"
-                      title="Title Come Here"
-                    >
-                      <i className="ti-zoom-in"></i>
-                    </a>
-                  </div>
-                </div>
-              </div>
-            </FadeInAnimate>
-            <FadeInAnimate className="col-lg-6 col-md-6 col-sm-6 m-b30 wow fadeInRight">
-              <div className="dlab-box portfolio-bx style2 project-media">
-                <div className="dlab-media dlab-img-overlay1 dlab-img-effect">
-                  <a href="javascript:void(0);">
-                    {" "}
-                    <img src="/images/gallery/gallery-6/pic4.jpg" alt="" />{" "}
-                  </a>
-                  <div className="overlay-bx">
-                    <a
-                      href="images/gallery/gallery-6/pic4.jpg"
-                      className="mfp-link"
-                      title="Title Come Here"
-                    >
-                      <i className="ti-zoom-in"></i>
-                    </a>
-                  </div>
-                </div>
-              </div>
-            </FadeInAnimate>
-          </div>
-        </div>
+        {project?.attributes && <Project projectData={project.attributes} />}
       </div>
-
-      {project?.attributes && <Project projectData={project.attributes} />}
-    </div>
+    </Fragment>
   )
 }
 
 export default ProjectDetails
-
 
 // export const getStaticProps: GetStaticProps = async ({ locale }) => {
 // 	return {
